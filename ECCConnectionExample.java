1bf86fae2358d0fd601d493be599f76e37ee405d
Marc de Klerk <deklerkmc@gmail.com>
2018-08-17 11:18:47 +0200

    added POST \/ecc\/add\/

--- ECCConnectionExample.java
+++ ECCConnectionExample.java
@@ -12,8 +12,6 @@ import org.json.simple.parser.JSONParser;
 
 public class ECCConnectionExample {
 
-	private final String USER_AGENT = "Mozilla/5.0";
-
   private final static String BASE_URL = "http://localhost:8083";
 
 	public static void main(String[] args) throws Exception {
@@ -30,6 +28,9 @@ public class ECCConnectionExample {
       System.out.println("exiting...");
       System.exit(0);
     }
+
+		final JSONObject curvepoint = http.sendPost();
+		System.out.println(curvepoint);
 	}
 
 	// HTTP GET request
@@ -51,6 +52,52 @@ public class ECCConnectionExample {
 
 	  JSONParser parser = new JSONParser();
     JSONObject jsonObject = (JSONObject) parser.parse(response.toString());
+
+		return jsonObject;
+	}
+
+	// HTTP POST request
+	private JSONObject sendPost() throws Exception {
+
+		URL obj = new URL(BASE_URL + "/ec/add/");
+		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+
+		// Create JSON input
+		JSONObject a = new JSONObject();
+		JSONObject b = new JSONObject();
+
+		a.put("x", "0x030644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd3");
+		a.put("y", "0x1a76dae6d3272396d0cbe61fced2bc532edac647851e3ac53ce1cc9c7e645a83");
+		b.put("x", "0x0000000000000000000000000000000000000000000000000000000000000001");
+		b.put("y", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd45");
+
+		JSONObject input = new JSONObject();
+		input.put("a", a);
+		input.put("b", b);
+
+		// Send post request
+		conn.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+		wr.writeBytes(input.toString());
+		wr.flush();
+		wr.close();
+
+		// Create response
+		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		// JSONify
+		JSONParser parser = new JSONParser();
+		JSONObject jsonObject = (JSONObject) parser.parse(response.toString());
 
 		return jsonObject;
 	}
